f486a18f8c3221803549fc3d99054d598dd2ea98
soham-pawar <sohamrpawar.2018@gmail.com>
2026-01-08 14:57:12 +0000

    container config done

---
+++ Basics/01_var.js
@@ -0,0 +1,10 @@
+const accountId = 400101
+let accountEmail = "coder@yahoo.com"
+var accountpass = "98765"
+accountcity = "Mumbai" //Can be done so but not suggested
+
+//accountId = 23
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountpass,accountcity])
