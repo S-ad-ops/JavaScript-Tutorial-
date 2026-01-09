1416856d0f2caadfe16989d3d87f62f4367d1fde
soham-pawar <sohamrpawar.2018@gmail.com>
2026-01-09 10:03:21 +0000

    DT

---
+++ Basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use alert;"
+
+//alert(6+8)  //availabe in browser not node.js or editor 
+
+//code should be easily readable 
+
+/* "Types Of Datatypes"
+number => 2 to power 53 
+bigint(not that usable)
+string = ""
+boolean => true or false 
+null => standalone value (empty)
+undefined => value not assigned 
+symbol => uniqueness 
+object  
+*/
+console.log(typeof "SOHA");
