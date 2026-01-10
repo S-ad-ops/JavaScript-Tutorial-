1c765db000c6f0806d1bd48c1d465ef2244ee9c8
soham-pawar <sohamrpawar.2018@gmail.com>
2026-01-10 10:04:51 +0000

    Conversion of datatypes

---
+++ Basics/03_conversionoperation.js
@@ -0,0 +1,13 @@
+let grade = 67
+
+console.log(grade);
+console.log(typeof grade);
+
+let value = String(grade)
+console.log(value);
+console.log(typeof value);
+//if the string does not have any number and is converted to number the output given will be NaN(not a number) eg., "33abc" => NaN
+//unll => 0
+//undefined => NaN
+//true => 1; false => 0;
+
