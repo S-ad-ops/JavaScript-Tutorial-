a709250f6da02580cf2105d013bc72188665aab2
soham-pawar <sohamrpawar.2018@gmail.com>
2026-01-10 10:23:56 +0000

    operations

--- Basics/03_conversionoperation.js
+++ Basics/03_conversionoperation.js
@@ -1,13 +1,23 @@
 let grade = 67
 
-console.log(grade);
-console.log(typeof grade);
+//console.log(grade);
+//console.log(typeof grade);
 
 let value = String(grade)
-console.log(value);
-console.log(typeof value);
+//console.log(value);
+//console.log(typeof value);
 //if the string does not have any number and is converted to number the output given will be NaN(not a number) eg., "33abc" => NaN
 //unll => 0
 //undefined => NaN
 //true => 1; false => 0;
+
+// OPERATIONS 
+let val = 3
+let negval = -val
+//console.log(negval);
+
+//console.log("1" + 2); //op=>12 
+//console.log(1 + "2");//op=>12
+//console.log("1"+2+2);//op=>122
+//console.log(1+2+"2");//op=>32   
 
