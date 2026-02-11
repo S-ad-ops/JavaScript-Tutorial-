7d2691122ab508089921aa2da0790c2b63994789
soham-pawar <sohamrpawar.2018@gmail.com>
2026-02-11 09:37:23 +0000

    #date

---
+++ Basics/07_dateabdtime.js
@@ -0,0 +1,14 @@
+// DATE 
+let date = new Date();
+/* 
+console.log(date.toString());   //.tostring() converts the date object to a string representation of the date and time.
+console.log(date.toISOString());  //.toISOString() converts the date object to a string in ISO 8601 format, which is a standardized format for representing date and time.
+console.log(date.toLocaleDateString());//.toLocaleDateString() converts the date object to a string representation of the date according to the local conventions of the user's locale.
+console.log(date.toLocaleTimeString());//.toLocaleTimeString() converts the date object to a string representation of the time according to the local conventions of the user's locale.
+*/
+console.log(typeof date);
+
+//Date declaration
+let mydate = new Date(2026, 1, 1);
+console.log(mydate.toDateString());
+
